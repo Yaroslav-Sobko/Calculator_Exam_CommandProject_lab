2bbed200e8ae29b74e017518f45c05e80e5b8173
Yaroslav-Sobko <yaroslav.sobko.pm.2020@lpnu.ua>
2022-09-10 17:40:42 +0300

    Add project files.

---
+++ AnalizerClassLibraryTests/AnalizerClassTests.cs
@@ -0,0 +1,28 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AnalizerClassLibraryTests
+{
+	[TestClass]
+	public class AnalizerClassTests
+	{
+		public TestContext TestContext { get; set; }
+		[DataSource("System.Data.SqlClient", @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\TestSymbols.mdf;Integrated Security=True;Connect Timeout=30", "Table_1", DataAccessMethod.Sequential)]
+		[TestMethod]
+		public void TestGetPrioriryMethod()
+		{
+			// Arrange
+			string symbol = (string)TestContext.DataRow["symbol"];
+			int expected = (int)TestContext.DataRow["expected"];
+			int actual;
+
+			// Actual
+			actual = AnalaizerClass.GetPriority(symbol);
+
+			// Assert
+			Assert.AreEqual(actual, expected);
+
+		}
+	}
+}
---
+++ AnalizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ae68f0d-d8ed-4112-aa92-b1c4fc99232e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
